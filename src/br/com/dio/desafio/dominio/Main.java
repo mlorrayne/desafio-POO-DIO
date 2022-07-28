e2b6f2f536c86b692427dcf14784db2d6bec8665
mlorrayne <lorrayne_pego@yahoo.com.br>
2022-07-28 19:05:01 -0300

    Final commit

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -21,11 +21,44 @@ public class Main {
         mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-
-
-        System.out.println(curso1);
+       /* System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devLorrayne = new Dev();
+        devLorrayne.setNome("Lorrayne");
+        devLorrayne.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devLorrayne.getConteudosInscritos());
+        devLorrayne.progredir();
+        devLorrayne.progredir();
+        System.out.println("---");
+        System.out.println("Conteúdos Inscritos" + devLorrayne.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devLorrayne.getConteudosConcluidos());
+        System.out.println("XP" + devLorrayne.calcularTotalXp());
+
+
+
+        System.out.println("---------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("---");
+        System.out.println("Conteúdos Inscritos" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devJoao.getConteudosConcluidos());
+        System.out.println("XP" + devJoao.calcularTotalXp());
+
 
     }
 }
