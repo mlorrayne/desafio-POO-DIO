dcab07d38fbebc73e95c2d14b538d0dd0315a5d7
mlorrayne <lorrayne_pego@yahoo.com.br>
2022-07-28 18:11:04 -0300

    Conceituação de Herança e Polimorfismo.

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -21,6 +21,8 @@ public class Main {
         mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
 
+
+
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
